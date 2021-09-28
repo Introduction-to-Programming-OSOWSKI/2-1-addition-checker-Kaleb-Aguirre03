5b7de5cca3789909b3b7bd9f63e9fae87f24e624
Kaleb-Aguirre03 <820930@mystma.org>
2021-09-28 09:13:52 -0500

    #1

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-#WRITE YOUR CODE HERE
+#Define the function addCheck() with if and else statements
+def addCheck(x, y, z):
+    if x + y == z:
+        return "True"
+
+    else:
+        return "False"
+
+#Print the function with 2, 2, and 4
+print (addCheck(2, 2, 4))
+
+#Print the function with 10, 3, and 13
+print (addCheck(10, 3, 13))
+
+#Print the function with 10, 3, and 30
+print (addCheck(10, 3, 30))
